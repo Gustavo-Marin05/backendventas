832167d4f872fc51011f8112f61912a4d7eb7101
gustavo <67645041g@gmail.com>
2025-07-19 10:39:54 -0400

    espero qeu si de

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -7,8 +7,8 @@ import { RolesGuard } from 'src/auth/guard/roles.guard';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 
 @Controller('product')
-@UseGuards(AuthGuard,RolesGuard)
- @Roles('ADMIN','USER')
+@UseGuards(AuthGuard, RolesGuard)
+@Roles('ADMIN', 'USER')
 export class ProductController {
   constructor(private readonly productService: ProductService) { }
 
@@ -33,15 +33,15 @@ export class ProductController {
 
   @Patch(':id')
   @Roles('ADMIN')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto,@Request() req) {
+  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto, @Request() req) {
     const userId = req.user.id
-    return this.productService.update(+id, updateProductDto,userId);
+    return this.productService.update(+id, updateProductDto, userId);
   }
 
   @Delete(':id')
   @Roles('ADMIN')
-  remove(@Param('id') id: string,@Request() req) {
+  remove(@Param('id') id: string, @Request() req) {
     const userId = req.user.id
-    return this.productService.remove(+id,userId);
+    return this.productService.remove(+id, userId);
   }
 }
