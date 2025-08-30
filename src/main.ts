7e756802346371fd11da4f341a4837449bcc427f
gustavo <67645041g@gmail.com>
2025-08-29 22:01:00 -0400

    cambio de puerto en variables de entorno

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
   app.enableCors({
     //origin: 'http://localhost:5173',
-    origin: 'https://tiendazo.vercel.app',
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
